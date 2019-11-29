dd909b1e730cc7c5dc3be21529c8d9e9c198f53f
wxr031 <wxr031@gmail.com>
2019-11-29 09:32:22 +0800

    finish registration

--- client.py
+++ client.py
@@ -1,68 +1,172 @@
 import sys
 import socket
-import tkinter, tkinter.ttk
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import CENTER, NORMAL, DISABLED, END
 
 client = None
-
-window = tkinter.Tk()
-window.title('CN Message')
-window.geometry('700x450')
 
 def connect():
 	global client
 	
 	if connect_button['text'] == 'Disconnect':
 		client.close()
-		host_input['state'] = tkinter.NORMAL
-		port_input['state'] = tkinter.NORMAL
+		host_input['state'] = NORMAL
+		port_input['state'] = NORMAL
 		connect_button['text'] = 'Connect'
+		status_text['text'] = ''
+		register_status_text['text'] = ''
+		tabs.tab(1, state = DISABLED)
 
 	else:
-		
-		host = host_var.get()
-		port = int(port_var.get())
-
 		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		
+		if len(host_var.get()) == 0 or len(port_var.get()) == 0:
+			status_text['text'] = 'Please provide host and port'
+			status_text['fg'] = 'red'
+			return
+
+		host = host_var.get()
+		
+		# check if the port value is a valid integer
+		try:
+			port = int(port_var.get())
+		except:
+			status_text['text'] = 'Invalid port number'
+			status_text['fg'] = 'red'
+			return
+
+		if port < 0 or port > 65535:
+			status_text['text'] = 'Invalid port number'
+			status_text['fg'] = 'red'
+			return
+	
 		try:
 			client.connect((host, port))
 		except:
 			status_text['text'] = 'Cannot connect to {}:{}'.format(host, port)
 			status_text['fg'] = 'red'
 			return
-		else:
-			status_text['text'] = 'Connection to {}:{} established'.format(host, port)
-			status_text['fg'] = 'green'
-			connect_button['text'] = 'Disconnect'
-			host_input['state'] = tkinter.DISABLED
-			port_input['state'] = tkinter.DISABLED
-		finally:
-			status_text.place(relx = 0.5, rely = 0.8, anchor = tkinter.CENTER)
+
+		status_text['text'] = 'Connection to {}:{} established'.format(host, port)
+		status_text['fg'] = 'green'
+		connect_button['text'] = 'Disconnect'
+		host_input['state'] = DISABLED
+		port_input['state'] = DISABLED
+		tabs.tab(1, state = NORMAL)
+
+def register():
+	
+	username = username_var.get()
+	password = password_var.get()
+
+	if len(username) == 0 or len(password) == 0:
+		register_status_text['text'] = 'Please provide username and password'
+		register_status_text['fg'] = 'red'
+		password_input.delete(0, END)
+		return
+	
+	if len(username) > 1024 or len(password) > 1024:
+		register_status_text['text'] = 'username and password must be less than 1024 characters'
+		register_status_text['fg'] = 'red'
+		password_input.delete(0, END)
+		return
+	
+	if not username.isalnum() or not password.isalnum():
+		register_status_text['text'] = 'username and password must only contains alphanumeric values'
+		register_status_text['fg'] = 'red'
+		username_input.delete(0, END)
+		password_input.delete(0, END)
+		return
+	
+	assert client is not None
+	client.send(b'REGISTER')
+
+	response = client.recv(16)
+	assert response == b'USER&PASS'
+	
+	client.send(username.encode() + b'&' + password.encode())
+	response = client.recv(16)
+	if response == b'OK':
+		register_status_text['text'] = 'Registration OK'
+		register_status_text['fg'] = 'green'
+		username_input.delete(0, END)
+		password_input.delete(0, END)
+
+	elif response == b'DUP':
+		register_status_text['text'] = 'Username already exist'
+		register_status_text['fg'] = 'red'
+		username_input.delete(0, END)
+		password_input.delete(0, END)
+
+window = tk.Tk()
+window.title('CN Message')
+window.geometry('700x450')
 
 
-title = tkinter.Label(window, text = 'CN Message', font = ('Courier', 48, 'bold'))
-title.place(relx = 0.5, rely = 0.2, anchor = tkinter.CENTER)
+# Tabs
+tabs = ttk.Notebook(window)
+tab_home = ttk.Frame(tabs)
+tab_register = ttk.Frame(tabs)
+tabs.add(tab_home, text = 'Home')
+tabs.add(tab_register, text = 'Registration', state = DISABLED)
 
-host_label = tkinter.Label(window, text = 'Host', font = ('Courier', 24))
-host_label.place(relx = 0.3, rely = 0.4, anchor = tkinter.CENTER)
+# User Interface
 
-port_label = tkinter.Label(window, text = 'Port', font = ('Courier', 24))
-port_label.place(relx = 0.7, rely = 0.4, anchor = tkinter.CENTER)
+# Home Page
+home_title = tk.Label(tab_home, text = 'CN Message', font = ('Inconsolata', 48, 'bold'))
+home_title.place(relx = 0.5, rely = 0.2, anchor = CENTER)
 
-host_var = tkinter.StringVar()
-port_var = tkinter.StringVar()
+host_label = tk.Label(tab_home, text = 'Host', font = ('Inconsolata', 16))
+host_label.place(relx = 0.3, rely = 0.4, anchor = CENTER)
 
-host_input = tkinter.Entry(width = 12, bd = 3, textvariable = host_var, font = ('Courier', 16))
-host_input.place(relx = 0.3, rely = 0.5, anchor = tkinter.CENTER)
+port_label = tk.Label(tab_home, text = 'Port', font = ('Inconsolata', 16))
+port_label.place(relx = 0.7, rely = 0.4, anchor = CENTER)
 
-port_input = tkinter.Entry(width = 12, bd = 3, textvariable = port_var, font = ('Courier', 16))
-port_input.place(relx = 0.7, rely = 0.5, anchor = tkinter.CENTER)
+host_var = tk.StringVar()
+port_var = tk.StringVar()
 
-column = tkinter.Label(window, text = ':', font = ('Courier', 16))
-column.place(relx = 0.5, rely = 0.5, anchor = tkinter.CENTER)
+host_input = tk.Entry(tab_home, width = 12, bd = 3, textvariable = host_var, font = ('Inconsolata', 14))
+host_input.place(relx = 0.3, rely = 0.5, anchor = CENTER)
 
-status_text = tkinter.Label(window, text = '')
+port_input = tk.Entry(tab_home, width = 12, bd = 3, textvariable = port_var, font = ('Inconsolata', 14))
+port_input.place(relx = 0.7, rely = 0.5, anchor = CENTER)
 
-connect_button = tkinter.Button(window, text = 'Connect', command = connect, font = ('Courier', 24, 'bold'))
-connect_button.place(relx = 0.5, rely = 0.65, anchor = tkinter.CENTER)
+column = tk.Label(tab_home, text = ':', font = ('Inconsolata', 16))
+column.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+
+status_text = tk.Label(tab_home, text = '')
+status_text.place(relx = 0.5, rely = 0.8, anchor = CENTER)
+
+connect_button = tk.Button(tab_home, text = 'Connect', command = connect, font = ('Inconsolata', 24, 'bold'))
+connect_button.place(relx = 0.5, rely = 0.65, anchor = CENTER)
+
+# Registration
+register_title = tk.Label(tab_register, text = 'Registration', font = ('Inconsolata', 24, 'bold'))
+register_title.place(relx = 0.5, rely = 0.2, anchor = CENTER)
+
+username_image = tk.PhotoImage(file = './images/username.png')
+username_image_label = tk.Label(tab_register, image = username_image)
+username_image_label.place(relx = 0.35, rely = 0.4, anchor = CENTER)
+username_var = tk.StringVar()
+username_input = tk.Entry(tab_register, width = 20, bd = 3, textvariable = username_var)
+username_input.focus()
+username_input.place(relx = 0.5, rely = 0.4, anchor = CENTER)
+
+password_image = tk.PhotoImage(file = './images/password.png')
+password_image_label = tk.Label(tab_register, image = password_image)
+password_image_label.place(relx = 0.35, rely = 0.5, anchor = CENTER)
+password_var = tk.StringVar()
+password_input = tk.Entry(tab_register, width = 20, bd = 3, textvariable = password_var)
+password_input.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+
+register_button = tk.Button(tab_register, text = 'Register', command = register, font = ('Inconsolata', 16, 'bold'))
+register_button.place(relx = 0.5, rely = 0.7, anchor = CENTER)
+
+register_status_text = tk.Label(tab_register, text = '')
+register_status_text.place(relx = 0.5, rely = 0.8, anchor = CENTER)
+
+# render tabs
+tabs.pack(expand = 1, fill = 'both')
 
 window.mainloop()
--- server.py
+++ server.py
@@ -1,15 +1,53 @@
 import sys
+import os
 import socket
+import dbm
+import hashlib
+import binascii
 
 online = set()
 
 host = 'localhost'
-port = int(sys.argv[0])
+port = int(sys.argv[1])
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen(5)
+def hash_password(password):
+	password = password.encode()
+	
+	salt = os.urandom(64)
+	password_hash = hashlib.pbkdf2_hmac('sha256', password, salt, 2048)
+	password_hash = salt + password_hash
+	return binascii.hexlify(password_hash)
 
-while True:
-	conn, (host, port) = server.accept()
-	online.add((host, port))
+def verify_password(password_hash, password):
+	password_hash = binascii.unhexlify(password_hash)
+	password = password.encode()
+
+	salt = password_hash[:64]
+	password_hash_verify = hashlib.pbkdf2_hmac('sha256', password, salt, 2048)
+	return password_hash_verify == password_hash[64:]
+
+	
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	server.bind((host, port))
+	server.listen(5)
+	while True:
+		conn, (host, port) = server.accept()
+		with conn:
+			while True:
+				online.add((host, port))
+				command = conn.recv(16)
+				if command == b'REGISTER':
+					conn.send(b'USER&PASS')
+					request = conn.recv(2049)
+					username, password = tuple(request.split(b'&'))
+					username = username.decode('ascii')
+					password = password.decode('ascii')
+					with dbm.open('auth', 'c') as db:
+						try:
+							db[username]
+							conn.send(b'DUP')
+						except KeyError:
+							db[username] = hash_password(password)
+							conn.send(b'OK')
