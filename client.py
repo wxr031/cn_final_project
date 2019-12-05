c46d2993486d1c8f0463f96e9af8f76c52f16ecc
wxr031 <wxr031@gmail.com>
2019-12-05 12:04:25 +0800

    add signin

--- client.py
+++ client.py
@@ -4,19 +4,24 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import CENTER, NORMAL, DISABLED, END
 
+CMD_MAX_LEN = 16
+
 client = None
 
 def connect():
 	global client
 	
 	if connect_button['text'] == 'Disconnect':
+		client.send(b'CLOSE')
 		client.close()
 		host_input['state'] = NORMAL
 		port_input['state'] = NORMAL
 		connect_button['text'] = 'Connect'
 		status_text['text'] = ''
 		register_status_text['text'] = ''
+		messaging_status_text['text'] = ''
 		tabs.tab(1, state = DISABLED)
+		tabs.tab(2, state = DISABLED)
 
 	else:
 		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -54,62 +59,115 @@ def connect():
 		host_input['state'] = DISABLED
 		port_input['state'] = DISABLED
 		tabs.tab(1, state = NORMAL)
+		tabs.tab(2, state = NORMAL)
 
-def register():
+def signup():
 	
 	username = username_var.get()
 	password = password_var.get()
+	password_validation = password_validation_var.get()
 
 	if len(username) == 0 or len(password) == 0:
 		register_status_text['text'] = 'Please provide username and password'
 		register_status_text['fg'] = 'red'
+		username_input.delete(0, END)
 		password_input.delete(0, END)
+		password_validation_input.delete(0, END)
 		return
 	
 	if len(username) > 1024 or len(password) > 1024:
 		register_status_text['text'] = 'username and password must be less than 1024 characters'
 		register_status_text['fg'] = 'red'
+		username_input.delete(0, END)
 		password_input.delete(0, END)
+		password_validation_input.delete(0, END)
 		return
 	
+	if password != password_validation:
+		register_status_text['text'] = 'Password double confirmation failed. Please enter your password again'
+		register_status_text['fg'] = 'red'
+		username_input.delete(0, END)
+		password_input.delete(0, END)
+		password_validation_input.delete(0, END)
+		return
+
 	if not username.isalnum() or not password.isalnum():
 		register_status_text['text'] = 'username and password must only contains alphanumeric values'
 		register_status_text['fg'] = 'red'
 		username_input.delete(0, END)
 		password_input.delete(0, END)
+		password_validation_input.delete(0, END)
 		return
 	
 	assert client is not None
-	client.send(b'REGISTER')
+	client.send(b'SIGNUP')
 
-	response = client.recv(16)
+	response = client.recv(CMD_MAX_LEN)
 	assert response == b'USER&PASS'
 	
 	client.send(username.encode() + b'&' + password.encode())
-	response = client.recv(16)
+	response = client.recv(CMD_MAX_LEN)
 	if response == b'OK':
 		register_status_text['text'] = 'Registration OK'
 		register_status_text['fg'] = 'green'
 		username_input.delete(0, END)
 		password_input.delete(0, END)
+		password_validation_input.delete(0, END)
 
 	elif response == b'DUP':
 		register_status_text['text'] = 'Username already exist'
 		register_status_text['fg'] = 'red'
 		username_input.delete(0, END)
 		password_input.delete(0, END)
+		password_validation_input.delete(0, END)
+
+def signin():
+	print('Sign in')
+	pass
+
+def sign_in_up_toggle():
+	if sign_in_up_button['text'] == 'Sign In':
+		register_title['text'] = 'Sign In'
+		register_button['command'] = signin
+		sign_in_up_title['text'] = 'Don\'t have an account?' 
+		sign_in_up_button['text'] = 'Sign Up'
+		sign_in_up_button.place(relx = 0.7, rely = 0.8, anchor = CENTER)
+		register_status_text['text'] = ''
+		password_validation_input.place_forget()
+		password_validation_image_label.place_forget()
+	
+	elif sign_in_up_button['text'] == 'Sign Up':
+		register_title['text'] = 'Sign Up'
+		register_button['command'] = signup
+		sign_in_up_title['text'] = 'Have an account?' 
+		sign_in_up_button['text'] = 'Sign In'
+		sign_in_up_button.place(relx = 0.6, rely = 0.8, anchor = CENTER)
+		register_status_text['text'] = ''
+		password_validation_input.place(relx = 0.5, rely = 0.6, anchor = CENTER)
+		password_validation_image_label.place(relx = 0.35, rely = 0.6, anchor = CENTER)
+
+	
+#def get_history(event):
+	#client.send(b'HISTORY')
+	#assert client.recv(CMD_MAX_LEN) == b'USER'
+	#client.send()
+
+def messaging():
+	pass
 
 window = tk.Tk()
 window.title('CN Message')
-window.geometry('700x450')
+window.geometry('1024x768')
 
 
 # Tabs
 tabs = ttk.Notebook(window)
 tab_home = ttk.Frame(tabs)
 tab_register = ttk.Frame(tabs)
+tab_messaging = ttk.Frame(tabs)
 tabs.add(tab_home, text = 'Home')
 tabs.add(tab_register, text = 'Registration', state = DISABLED)
+tabs.add(tab_messaging, text = 'Messaging', state = NORMAL)
 
 # User Interface
 
@@ -117,39 +175,38 @@ tabs.add(tab_register, text = 'Registration', state = DISABLED)
 home_title = tk.Label(tab_home, text = 'CN Message', font = ('Inconsolata', 48, 'bold'))
 home_title.place(relx = 0.5, rely = 0.2, anchor = CENTER)
 
-host_label = tk.Label(tab_home, text = 'Host', font = ('Inconsolata', 16))
-host_label.place(relx = 0.3, rely = 0.4, anchor = CENTER)
-
-port_label = tk.Label(tab_home, text = 'Port', font = ('Inconsolata', 16))
-port_label.place(relx = 0.7, rely = 0.4, anchor = CENTER)
+host_label = tk.Label(tab_home, text = 'Host', font = ('Inconsolata', 24))
+host_label.place(relx = 0.3, rely = 0.425, anchor = CENTER)
+port_label = tk.Label(tab_home, text = 'Port', font = ('Inconsolata', 24))
+port_label.place(relx = 0.7, rely = 0.425, anchor = CENTER)
 
 host_var = tk.StringVar()
 port_var = tk.StringVar()
 
-host_input = tk.Entry(tab_home, width = 12, bd = 3, textvariable = host_var, font = ('Inconsolata', 14))
+host_input = tk.Entry(tab_home, width = 12, bd = 3, textvariable = host_var, font = ('Inconsolata', 18))
 host_input.place(relx = 0.3, rely = 0.5, anchor = CENTER)
 
-port_input = tk.Entry(tab_home, width = 12, bd = 3, textvariable = port_var, font = ('Inconsolata', 14))
+port_input = tk.Entry(tab_home, width = 12, bd = 3, textvariable = port_var, font = ('Inconsolata', 18))
 port_input.place(relx = 0.7, rely = 0.5, anchor = CENTER)
 
-column = tk.Label(tab_home, text = ':', font = ('Inconsolata', 16))
+column = tk.Label(tab_home, text = ':', font = ('Inconsolata', 24))
 column.place(relx = 0.5, rely = 0.5, anchor = CENTER)
 
-status_text = tk.Label(tab_home, text = '')
+status_text = tk.Label(tab_home, text = '', font = ('Inconsolata', 16, 'bold'))
 status_text.place(relx = 0.5, rely = 0.8, anchor = CENTER)
 
 connect_button = tk.Button(tab_home, text = 'Connect', command = connect, font = ('Inconsolata', 24, 'bold'))
 connect_button.place(relx = 0.5, rely = 0.65, anchor = CENTER)
 
 # Registration
-register_title = tk.Label(tab_register, text = 'Registration', font = ('Inconsolata', 24, 'bold'))
+register_title = tk.Label(tab_register, text = 'Sign Up', font = ('Inconsolata', 36, 'bold'))
 register_title.place(relx = 0.5, rely = 0.2, anchor = CENTER)
 
 username_image = tk.PhotoImage(file = './images/username.png')
 username_image_label = tk.Label(tab_register, image = username_image)
 username_image_label.place(relx = 0.35, rely = 0.4, anchor = CENTER)
 username_var = tk.StringVar()
-username_input = tk.Entry(tab_register, width = 20, bd = 3, textvariable = username_var)
+username_input = tk.Entry(tab_register, width = 32, bd = 3, textvariable = username_var)
 username_input.focus()
 username_input.place(relx = 0.5, rely = 0.4, anchor = CENTER)
 
@@ -157,14 +214,52 @@ password_image = tk.PhotoImage(file = './images/password.png')
 password_image_label = tk.Label(tab_register, image = password_image)
 password_image_label.place(relx = 0.35, rely = 0.5, anchor = CENTER)
 password_var = tk.StringVar()
-password_input = tk.Entry(tab_register, width = 20, bd = 3, textvariable = password_var)
+password_input = tk.Entry(tab_register, width = 32, bd = 3, textvariable = password_var)
 password_input.place(relx = 0.5, rely = 0.5, anchor = CENTER)
 
-register_button = tk.Button(tab_register, text = 'Register', command = register, font = ('Inconsolata', 16, 'bold'))
+password_validation_image = tk.PhotoImage(file = './images/check.png')
+password_validation_image_label = tk.Label(tab_register, image = password_validation_image)
+password_validation_image_label.place(relx = 0.35, rely = 0.6, anchor = CENTER)
+password_validation_var = tk.StringVar()
+password_validation_input = tk.Entry(tab_register, width = 32, bd = 3, textvariable = password_validation_var)
+password_validation_input.place(relx = 0.5, rely = 0.6, anchor = CENTER)
+
+register_button = tk.Button(tab_register, text = 'Submit', command = signup, font = ('Inconsolata', 24, 'bold'))
 register_button.place(relx = 0.5, rely = 0.7, anchor = CENTER)
 
-register_status_text = tk.Label(tab_register, text = '')
-register_status_text.place(relx = 0.5, rely = 0.8, anchor = CENTER)
+sign_in_up_title = tk.Label(tab_register, text = 'Have an account?', font = ('Inconsolata', 18))
+sign_in_up_title.place(relx = 0.4, rely = 0.8, anchor = CENTER)
+
+sign_in_up_button = tk.Button(tab_register, text = 'Sign In', command = sign_in_up_toggle, font = ('Inconsolata', 16, 'bold'))
+sign_in_up_button.place(relx = 0.6, rely = 0.8, anchor = CENTER)
+
+register_status_text = tk.Label(tab_register, text = '', font = ('Inconsolata', 16, 'bold'))
+register_status_text.place(relx = 0.5, rely = 0.9, anchor = CENTER)
+
+
+# Messaging
+#messaging_title = tk.Label(tab_messaging, text = 'Messaging', font = ('Inconsolata', 32, 'bold'))
+#messaging_title.place(relx = 0.5, rely = 0.15, anchor = CENTER)
+#
+#dest_label = tk.Label(tab_messaging, text = 'Send to : ', font = ('Inconsolata', 24))
+#dest_label.place(relx = 0.15, rely = 0.25, anchor = CENTER)
+#dest_var = tk.StringVar()
+#dest_input = tk.Entry(tab_messaging, width = 48, bd = 3, textvariable = dest_var, font = ('Inconsolata', 18))
+#dest_input.place(relx = 0.6, rely = 0.25, anchor = CENTER)
+#
+#history_label = tk.Label(tab_messaging, text = 'Send to : ', font = ('Inconsolata', 24))
+#history_label.place(relx = 0.15, rely = 0.25, anchor = CENTER)
+#history_combo = ttk.Combobox(tab_messaging, width = 48, state='readonly', font = ('Inconsolata', 18))
+#history_combo['values'] = ('---history---')
+#history_combo.current(0)
+#history_combo.bind('<Button-1>', get_history)
+#history_combo.place(relx = 0.6, rely = 0.35, anchor = CENTER)
+#
+#messaging_button = tk.Button(tab_messaging, text = 'Send', command = messaging, font = ('Inconsolata', 24, 'bold'))
+#messaging_button.place(relx = 0.5, rely = 0.7, anchor = CENTER)
+#
+#messaging_status_text = tk.Label(tab_messaging, text = '')
+#messaging_status_text.place(relx = 0.5, rely = 0.8, anchor = CENTER)
 
 # render tabs
 tabs.pack(expand = 1, fill = 'both')
--- server.py
+++ server.py
@@ -1,14 +1,20 @@
 import sys
 import os
 import socket
-import dbm
+import json
 import hashlib
 import binascii
 
 online = set()
 
+USER_LEN_MAX = 1024
+PASS_LEN_MAX = 1024
+
 host = 'localhost'
 port = int(sys.argv[1])
+
+auth_file = 'auth.json'
+hist_file = 'hist.json'
 
 def hash_password(password):
 	password = password.encode()
@@ -16,7 +22,7 @@ def hash_password(password):
 	salt = os.urandom(64)
 	password_hash = hashlib.pbkdf2_hmac('sha256', password, salt, 2048)
 	password_hash = salt + password_hash
-	return binascii.hexlify(password_hash)
+	return binascii.hexlify(password_hash).decode('ascii')
 
 def verify_password(password_hash, password):
 	password_hash = binascii.unhexlify(password_hash)
@@ -26,28 +32,67 @@ def verify_password(password_hash, password):
 	password_hash_verify = hashlib.pbkdf2_hmac('sha256', password, salt, 2048)
 	return password_hash_verify == password_hash[64:]
 
-	
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	server.bind((host, port))
-	server.listen(5)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((host, port))
+server.listen(5)
+while True:
+	conn, (host, port) = server.accept()
 	while True:
-		conn, (host, port) = server.accept()
-		with conn:
-			while True:
-				online.add((host, port))
-				command = conn.recv(16)
-				if command == b'REGISTER':
-					conn.send(b'USER&PASS')
-					request = conn.recv(2049)
-					username, password = tuple(request.split(b'&'))
-					username = username.decode('ascii')
-					password = password.decode('ascii')
-					with dbm.open('auth', 'c') as db:
-						try:
-							db[username]
-							conn.send(b'DUP')
-						except KeyError:
-							db[username] = hash_password(password)
-							conn.send(b'OK')
+		command = conn.recv(16)
+		print(command)
+		if command == b'SIGNUP':
+			conn.send(b'USER&PASS')
+			request = conn.recv(USER_LEN_MAX + 1 + PASS_LEN_MAX)
+			username, password = tuple(request.split(b'&'))
+			username = username.decode('ascii')
+			password = password.decode('ascii')
+			if not os.path.exists(auth_file):
+				with open(auth_file, 'w') as fw:
+					json.dump({}, fw)
+			with open(auth_file, 'r') as fr:
+				auth = json.load(fr)
+				print(auth)
+				if username in auth:
+					conn.send(b'DUP')
+				else:
+					auth[username] = hash_password(password)
+					conn.send(b'OK')
+					with open(auth_file, 'w') as fw:
+						json.dump(auth, fw)
+
+		elif command == b'SIGNIN':
+			conn.send(b'USER&PASS')
+			request = conn.recv(USER_LEN_MAX + 1 + PASS_LEN_MAX)
+			username, password = tuple(request.split(b'&'))
+			username = username.decode('ascii')
+			password = password.decode('ascii')
+			if not os.path.exists(auth_file):
+				with open(auth_file, 'w') as fw:
+					json.dump({}, fw)
+			with open(auth_file, 'r') as fr:
+				auth = json.load(fr)
+				password_hash = auth[username]
+				if verify_password(password_hash, password):
+					conn.send(b'OK')
+				else:
+					conn.send(b'REJ')
+			
+		#elif command == b'HISTORY':
+			#conn.send(b'USER')
+			#username = conn.recv(USER_LEN_MAX)
+			#username = username.decode('ascii')
+			#if not os.path.exists(hist_file):
+				#with open(hist_file, 'w') as fw:
+					#json.dump({}, fw)
+			#with open(hist_file, 'r') as fr:
+				#hists = json.load(fr)[username]
+				#conn.send(json.dumps(hist))
+				
+
+
+		elif command == b'CLOSE':
+			conn.close()
+			break
+
+socket.close()
